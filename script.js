5468021270e56071ec557650f26f666cb42448ef
mohammadfahed <152049314+mohammadfahed@users.noreply.github.com>
2024-12-19 20:44:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const target = document.getElementsByClassName('btn');
+function moveTarget() {
+
+
+}
+click.addEventListener('click', () => {
+    window.open("https://maps.app.goo.gl/A8yPjGaJgTMoZggk8")
+
+})
